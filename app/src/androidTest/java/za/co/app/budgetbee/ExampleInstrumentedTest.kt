faa3b8bfdb5f6fc135a8c1afedee2f347c844633
joemalebe <jtekniq@gmail.com>
2019-11-27 21:55:15 +0200

    Add image resources

--- app/src/androidTest/java/za/co/app/budgetbee/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/za/co/app/budgetbee/ExampleInstrumentedTest.kt
@@ -1,8 +1,8 @@
-package com.example.budgetbee
+package za.co.app.budgetbee
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.budgetbee", appContext.packageName)
+        assertEquals("za.co.app.budgetbee", appContext.packageName)
     }
 }
