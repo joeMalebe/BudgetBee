746e92b9c0cca0c74b727f0bc621ab4146dff652
joemalebe <jtekniq@gmail.com>
2020-01-22 21:21:49 +0200

    Add expense types screen

--- app/src/main/java/za/co/app/budgetbee/LandingActivity.kt
+++ app/src/main/java/za/co/app/budgetbee/LandingActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_landing.*
+import za.co.app.budgetbee.ui.AddExpenseActivity
 
 class LandingActivity : AppCompatActivity() {
 
@@ -17,5 +19,18 @@ class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+        buildScreen()
     }
+
+    private fun buildScreen() {
+        val addExpenseButton = add_expense_fab
+        val addIncomeButton = add_income_fab
+
+        addExpenseButton.setOnClickListener({
+            startActivity(Intent(this, AddExpenseActivity::class.java))
+        })
+
+    }
+
+
 }
