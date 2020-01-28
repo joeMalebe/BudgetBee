b2dc119b6a118a4778e5dbb80ef776f04b135f25
joemalebe <jtekniq@gmail.com>
2020-01-28 19:33:07 +0200

    create database with transaction categories table #2

--- app/src/main/java/za/co/app/budgetbee/model/BudgetBeeDatabase.kt
+++
@@ -1,9 +0,0 @@
-package za.co.app.budgetbee.model
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = arrayOf(TransactionCategory::class), version = 1)
-abstract class BudgetBeeDatabase : RoomDatabase() {
-    abstract fun transactionCategoryDao(): TransactionCategoryDao
-}
