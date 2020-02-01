b36304413a561714871548e551439f5f4a723edb
joemalebe <jtekniq@gmail.com>
2020-02-01 20:33:52 +0200

    Add date picker dialogue functionality

--- app/src/main/java/za/co/app/budgetbee/ui/AddTransactionActivity.kt
+++ app/src/main/java/za/co/app/budgetbee/ui/AddTransactionActivity.kt
@@ -1,12 +1,16 @@
 package za.co.app.budgetbee.ui
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_transaction.*
 import za.co.app.budgetbee.R
 import za.co.app.budgetbee.data.model.TransactionCategory
+import za.co.app.budgetbee.utils.getDateStringByFormat
+import java.util.*
 
 class AddTransactionActivity : AppCompatActivity() {
 
@@ -28,5 +32,29 @@ class AddTransactionActivity : AppCompatActivity() {
         val transactionCategory =
             intent.getParcelableExtra<TransactionCategory>(EXTRA_TRANSACTION_CATEGORY)
         Toast.makeText(this, transactionCategory.transactionCategoryName, Toast.LENGTH_SHORT).show()
+
+        val calendar = Calendar.getInstance()
+        input_date.setText(calendar.getDateStringByFormat())
+        input_date.setOnClickListener {
+            setupDateDialogue(calendar)
+        }
+    }
+
+    fun setupDateDialogue(calendar: Calendar) {
+        val year = calendar.get(Calendar.YEAR)
+        val monthOfYear = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(
+            this,
+            ({ view, selectedYear, selectedMonth, selectedDay ->
+                calendar.set(selectedYear, selectedMonth, selectedDay)
+                input_date.setText(calendar.getDateStringByFormat())
+                Toast.makeText(
+                    view.context,
+                    "$selectedYear/${selectedMonth + 1}/$selectedDay",
+                    Toast.LENGTH_LONG
+                ).show()
+            }), year, monthOfYear, dayOfMonth
+        ).show()
     }
 }
