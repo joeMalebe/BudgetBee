c934bc4abb1f870758d8024c9d4bf84898aaa833
joemalebe <jtekniq@gmail.com>
2020-02-07 18:59:17 +0200

    create database repo contract

---
+++ app/src/main/java/za/co/app/budgetbee/data/model/repository/IDatabaseRepository.kt
@@ -0,0 +1,25 @@
+package za.co.app.budgetbee.data.model.repository
+
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import za.co.app.budgetbee.data.model.Transaction
+import za.co.app.budgetbee.data.model.TransactionCategory
+
+interface IDatabaseRepository {
+    fun getAllTransactionCategories(): Observable<List<TransactionCategory>>
+
+    fun getTransactions(): Observable<List<Transaction>>
+
+    fun getTransactionsCategoryByName(transactionCategoryName: String): Single<TransactionCategory>
+
+    fun insertTransactionCategory(transactionCategory: TransactionCategory): Completable
+
+    fun insertAllTransactionCategory(transactionCategory: Array<TransactionCategory>): Completable
+
+    fun insertTransaction(transaction: Transaction): Completable
+
+    fun deleteTransactionCategory(transactionCategory: TransactionCategory): Completable
+
+    fun updateTransactionCategory(transactionCategory: TransactionCategory): Completable
+}
