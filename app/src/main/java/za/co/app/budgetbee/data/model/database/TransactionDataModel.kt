045ee6daaade42fdd3695ad8ee64afe670f43293
joemalebe <jtekniq@gmail.com>
2020-02-08 16:53:27 +0200

    disable lint pakage error

--- app/src/main/java/za/co/app/budgetbee/data/model/database/TransactionDataModel.kt
+++ app/src/main/java/za/co/app/budgetbee/data/model/database/TransactionDataModel.kt
@@ -2,18 +2,18 @@ package za.co.app.budgetbee.data.model.database
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 
 @Entity(
-    foreignKeys = arrayOf(
-        ForeignKey(
-            entity = TransactionCategoryDataModel::class,
-            parentColumns = arrayOf("transactionCategoryId"),
-            childColumns = arrayOf("transactionCategoryId"),
-            onDelete = ForeignKey.CASCADE
-        )
-    )
+    indices = arrayOf(Index(value = arrayOf("transactionCategoryId"), unique = true)),
+    foreignKeys = [ForeignKey(
+        entity = TransactionCategoryDataModel::class,
+        parentColumns = arrayOf("transactionCategoryId"),
+        childColumns = arrayOf("transactionCategoryId"),
+        onDelete = ForeignKey.CASCADE
+    )]
 )
 data class TransactionDataModel(
     val transactionDate: Long,
