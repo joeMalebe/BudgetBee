f5c2e4deab8448c62e2e08572c6544ff0e5e70e7
joemalebe <jtekniq@gmail.com>
2020-02-09 13:52:45 +0200

    make transaction db indecies not unique

--- app/src/main/java/za/co/app/budgetbee/base/BaseObserver.kt
+++ app/src/main/java/za/co/app/budgetbee/base/BaseObserver.kt
@@ -6,19 +6,20 @@ import io.reactivex.disposables.Disposable
 
 abstract class BaseObserver<T> : Observer<T> {
     val TAG = BaseObserver::class.simpleName
+
     override fun onComplete() {
         Log.d(TAG, "onComplete called")
     }
 
-    override fun onSubscribe(d: Disposable) {
-        Log.d(TAG, "onSubscribe called with disposable $d")
+    override fun onSubscribe(disposable: Disposable) {
+        Log.d(TAG, "onSubscribe called with disposable $disposable")
     }
 
-    abstract override fun onNext(t: T)
+    abstract override fun onNext(value: T)
 
 
-    override fun onError(e: Throwable) {
-        Log.d(TAG, "onError called with ${e.message}")
+    override fun onError(error: Throwable) {
+        Log.d(TAG, "onError called with ${error.message}")
     }
 
 }
