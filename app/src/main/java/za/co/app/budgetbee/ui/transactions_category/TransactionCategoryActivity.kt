e8394d4a64daa5932920477a07adc1da1df54261
joemalebe <jtekniq@gmail.com>
2020-02-12 19:17:11 +0200

    implement baseView in activity

--- app/src/main/java/za/co/app/budgetbee/ui/transactions_category/TransactionCategoryActivity.kt
+++ app/src/main/java/za/co/app/budgetbee/ui/transactions_category/TransactionCategoryActivity.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_transaction_category.*
 import za.co.app.budgetbee.R
 import za.co.app.budgetbee.base.AppCompatBaseActivity
+import za.co.app.budgetbee.base.IBaseView
 import za.co.app.budgetbee.ui.transaction.AddTransactionPagerAdapter
 
-class TransactionCategoryActivity : AppCompatBaseActivity() {
+class TransactionCategoryActivity : AppCompatBaseActivity(), IBaseView {
+
+    lateinit var addIncomeButton: FloatingActionButton
 
     companion object {
         fun getStartIntent(context: Context): Intent {
@@ -20,19 +24,16 @@ class TransactionCategoryActivity : AppCompatBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_category)
-        buildScreen()
+        displayScreen()
     }
 
-    private fun buildScreen() {
+    override fun displayScreen() {
         val transactionViewPager = transaction_view_pager
         val transactionTabs = transaction_type_tab_layout
         val addIncomeButton = add_income_fab
+
         addIncomeButton.setOnClickListener {
-            startActivity(
-                AddTransactionCategoryActivity.getStartIntent(
-                    it.context
-                )
-            )
+            navigateToAddTransactionCategory()
         }
         transactionViewPager.adapter =
             AddTransactionPagerAdapter(
@@ -40,6 +41,14 @@ class TransactionCategoryActivity : AppCompatBaseActivity() {
                 BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
             )
         transactionTabs.setupWithViewPager(transactionViewPager)
+    }
+
+    fun navigateToAddTransactionCategory() {
+        startActivity(
+            AddTransactionCategoryActivity.getStartIntent(
+                this
+            )
+        )
     }
 }
 
