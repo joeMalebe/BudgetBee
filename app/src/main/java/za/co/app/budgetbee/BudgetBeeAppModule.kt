6baa4e7ad6647d023871597e6bc18640c2e54655
joemalebe <jtekniq@gmail.com>
2020-02-12 21:59:08 +0200

    inject presenter into view

--- app/src/main/java/za/co/app/budgetbee/BudgetBeeAppModule.kt
+++ app/src/main/java/za/co/app/budgetbee/BudgetBeeAppModule.kt
@@ -7,6 +7,7 @@ import za.co.app.budgetbee.data.model.database.BudgetBeeDoa
 import za.co.app.budgetbee.data.repository.TransactionsRepository
 import za.co.app.budgetbee.ui.landing.ILandingMvp
 import za.co.app.budgetbee.ui.landing.LandingPresenter
+import za.co.app.budgetbee.ui.transactions_category.ITransactionCategoryMvp
 import javax.inject.Singleton
 
 class BudgetBeeAppModule(val application: BudgetBeeApplication) {
@@ -29,6 +30,14 @@ class BudgetBeeAppModule(val application: BudgetBeeApplication) {
     @Singleton
     fun landingActivityPresenter(transactionsRepository: TransactionsRepository): ILandingMvp.Presenter {
         return LandingPresenter(
+            transactionsRepository
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun transactionCategoryAddCategoryActivityPresenter(transactionsRepository: TransactionsRepository): ITransactionCategoryMvp.Presenter {
+        return za.co.app.budgetbee.ui.transactions_category.TransactionCategoryAddCategoryPresenter(
             transactionsRepository
         )
     }
