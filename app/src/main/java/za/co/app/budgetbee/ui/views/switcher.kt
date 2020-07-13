447e5f34ba0da00692d1f3f5f341a5e3dc6c1784
joemalebe <jtekniq@gmail.com>
2020-07-13 20:19:56 +0200

    create initial switcher layout

---
+++ app/src/main/java/za/co/app/budgetbee/ui/views/switcher.kt
@@ -0,0 +1,9 @@
+package za.co.app.budgetbee.ui.views
+
+import android.content.Context
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Switcher(context: Context) : ConstraintLayout(context) {
+
+    constructor(context: Context, switcherValues: ArrayList<String>) : this(context)
+}
