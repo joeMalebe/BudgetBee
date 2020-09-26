29cc8d2a396fa53ef1cea02c675e9888bc8ca40e
joemalebe <jtekniq@gmail.com>
2020-09-26 16:15:21 +0200

    year switcher update year number

--- app/src/main/java/za/co/app/budgetbee/ui/views/year_switcher.kt
+++ app/src/main/java/za/co/app/budgetbee/ui/views/year_switcher.kt
@@ -2,24 +2,41 @@ package za.co.app.budgetbee.ui.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.layout_year_switcher.view.*
+import za.co.app.budgetbee.R
 import java.util.*
 
-class YearSwitcher @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null, defStyleRef: Int = 0) :
-        View(context, attributeSet, defStyleRef) {
+class YearSwitcher @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, dyfStyleAttr: Int = 0) :
+        ConstraintLayout(context, attrs, dyfStyleAttr) {
 
-    val publishYear = PublishSubject.create<Int>()
+    private val publishYear = PublishSubject.create<Int>()
+    var year = 0
 
-    fun init() {
-        val year = Calendar.getInstance()[Calendar.YEAR]
-        year_text.setText(year)
+    fun init(calendar: Calendar) {
+        LayoutInflater.from(context).inflate(R.layout.layout_year_switcher, this)
+
+        year = calendar[Calendar.YEAR]
+        year_text.text = year.toString()
         left_selector.setOnClickListener {
-            publishYear.onNext(year.dec())
+            year = year.dec()
+            publishYearAndUpdateText()
         }
         right_selector.setOnClickListener {
-            publishYear.onNext(year.inc())
+            year = year.inc()
+            publishYearAndUpdateText()
         }
+    }
+
+    private fun publishYearAndUpdateText() {
+        publishYear.onNext(year)
+        year_text.text = year.toString()
+    }
+
+    fun getSelectedYear(): Observable<Int> {
+        return publishYear.hide()
     }
 }
