64e86308f11050cb99c7fa585367cc0a1dd901aa
joemalebe <jtekniq@gmail.com>
2020-10-09 21:09:58 +0200

    pre populate transaction for editing

--- app/src/main/java/za/co/app/budgetbee/ui/add_transaction/AddTransactionActivity.kt
+++ app/src/main/java/za/co/app/budgetbee/ui/add_transaction/AddTransactionActivity.kt
@@ -4,9 +4,11 @@ import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_add_transaction.*
 import za.co.app.budgetbee.R
 import za.co.app.budgetbee.base.AppCompatBaseActivity
+import za.co.app.budgetbee.data.model.domain.Transaction
 import za.co.app.budgetbee.data.model.domain.TransactionCategory
 import za.co.app.budgetbee.data.model.domain.TransactionCategoryType
 import za.co.app.budgetbee.ui.landing.LandingActivity
@@ -16,15 +18,26 @@ import javax.inject.Inject
 
 class AddTransactionActivity : AppCompatBaseActivity(), IAddTransactionMvp.View {
 
+    private lateinit var inputDate: TextInputEditText
+    private lateinit var inputAmount: TextInputEditText
+
     @Inject
     lateinit var presenter: IAddTransactionMvp.Presenter
 
     companion object {
         const val EXTRA_TRANSACTION_CATEGORY = "EXTRA_TRANSACTION_CATEGORY"
+        const val EXTRA_TRANSACTION = "EXTRA_TRANSACTION"
 
         fun getStartIntent(context: Context?, transactionCategory: TransactionCategory): Intent {
             val intent = Intent(context, AddTransactionActivity::class.java)
             intent.putExtra(EXTRA_TRANSACTION_CATEGORY, transactionCategory)
+            return intent
+        }
+
+        fun getStartIntent(context: Context?, transactionCategory: TransactionCategory, transaction: Transaction): Intent {
+            val intent = Intent(context, AddTransactionActivity::class.java)
+            intent.putExtra(EXTRA_TRANSACTION_CATEGORY, transactionCategory)
+            intent.putExtra(EXTRA_TRANSACTION, transaction)
             return intent
         }
     }
@@ -33,6 +46,8 @@ class AddTransactionActivity : AppCompatBaseActivity(), IAddTransactionMvp.View 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transaction)
         presenter.attachView(this)
+        inputAmount = input_amount
+        inputDate = input_date
         displayScreen()
     }
 
@@ -64,7 +79,7 @@ class AddTransactionActivity : AppCompatBaseActivity(), IAddTransactionMvp.View 
     override fun addTransaction(calendar: Calendar, transactionCategory: TransactionCategory) {
         val date = calendar.timeInMillis
         val description = getTransactionDescription(transactionCategory.transactionCategoryType)
-        val amount = input_amount.text.toString().toDouble()
+        val amount = inputAmount.text.toString().toDouble()
 
         presenter.submitTransaction(
             date,
@@ -86,10 +101,17 @@ class AddTransactionActivity : AppCompatBaseActivity(), IAddTransactionMvp.View 
     override fun displayScreen() {
         val transactionCategory =
             intent.getParcelableExtra<TransactionCategory>(EXTRA_TRANSACTION_CATEGORY)
+        if(intent.hasExtra(EXTRA_TRANSACTION)){
+            val transaction = intent.getParcelableExtra<Transaction>(EXTRA_TRANSACTION)
+            inputAmount.setText(transaction.transactionAmount.toString())
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = transaction.transactionDate
+            inputDate.setText(calendar.getDateStringByFormat("dd MMMM yyyy"))
 
+        }
         val calendar = Calendar.getInstance()
-        input_date.setText(calendar.getDateStringByFormat())
-        input_date.setOnClickListener {
+        inputDate.setText(calendar.getDateStringByFormat())
+        inputDate.setOnClickListener {
             setupDateDialogue(calendar)
         }
 
