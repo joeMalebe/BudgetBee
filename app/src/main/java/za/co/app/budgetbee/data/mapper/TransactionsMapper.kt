20559e99b8545b71c6ed0f1d70e80a67b972abc9
joemalebe <jtekniq@gmail.com>
2020-10-10 14:46:33 +0200

    add transaction to model mapper

--- app/src/main/java/za/co/app/budgetbee/data/mapper/TransactionsMapper.kt
+++ app/src/main/java/za/co/app/budgetbee/data/mapper/TransactionsMapper.kt
@@ -8,21 +8,35 @@ import za.co.app.budgetbee.data.model.domain.TransactionCategory
 class TransactionsMapper {
 
     companion object {
-        fun mapModelToTransactions(modelIst: List<TransactionDataModel>): ArrayList<Transaction> {
+        fun mapModelListToTransactionsList(modelIst: List<TransactionDataModel>): ArrayList<Transaction> {
             val transactionsList: ArrayList<Transaction> = arrayListOf()
             modelIst.forEach { model ->
                 transactionsList.add(
-                        Transaction(
-                                model.transactionId,
-                                model.transactionDate,
-                                model.transactionDescription,
-                                model.transactionAmount,
-                                model.transactionCategoryId,
-                                model.transactionCategoryName
-                        )
+                        mapModelToTransaction(model)
                 )
             }
             return transactionsList
+        }
+
+        private fun mapModelToTransaction(model: TransactionDataModel): Transaction {
+            return Transaction(
+                    model.transactionId,
+                    model.transactionDate,
+                    model.transactionDescription,
+                    model.transactionAmount,
+                    model.transactionCategoryId,
+                    model.transactionCategoryName
+            )
+        }
+
+        private fun mapModelToTransaction(transaction: Transaction): TransactionDataModel {
+            return TransactionDataModel(
+                    transaction.transactionDate,
+                    transaction.transactionDescription,
+                    transaction.transactionAmount,
+                    transaction.transactionCategoryId,
+                    transaction.transactionCategoryName
+            )
         }
 
         fun mapModelToTransactionCategory(modelList: List<TransactionCategoryDataModel>): ArrayList<TransactionCategory> {
