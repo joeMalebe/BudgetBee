8f26975eeeb604986e9707bec55aafa1a5dc418b
joemalebe <jtekniq@gmail.com>
2020-10-12 21:30:35 +0200

    add get intent for edit transaction activity

--- app/src/main/java/za/co/app/budgetbee/ui/edit_transaction/EditTransactionActivity.kt
+++ app/src/main/java/za/co/app/budgetbee/ui/edit_transaction/EditTransactionActivity.kt
@@ -1,10 +1,23 @@
 package za.co.app.budgetbee.ui.edit_transaction
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import za.co.app.budgetbee.R
+import za.co.app.budgetbee.data.model.domain.Transaction
 
-class EditTransactionActivity : AppCompatActivity(),IEditTransactionMvp.View {
+class EditTransactionActivity : AppCompatActivity(), IEditTransactionMvp.View {
+
+    companion object {
+        private val EXTRA_TRANSACTION = "EXTRA_TRANSACTION"
+
+        fun getStartIntent(context: Context, transaction: Transaction): Intent {
+            val intent = Intent(context, EditTransactionActivity::class.java)
+            intent.putExtra(EXTRA_TRANSACTION, transaction)
+            return intent
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
