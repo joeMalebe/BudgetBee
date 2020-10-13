16513f4d0b257f398c98059be200dfc7cee788e7
joemalebe <jtekniq@gmail.com>
2020-10-13 19:24:13 +0200

    create edit transaction presenter shell

---
+++ app/src/main/java/za/co/app/budgetbee/ui/edit_transaction/EditTransactionPresenter.kt
@@ -0,0 +1,15 @@
+package za.co.app.budgetbee.ui.edit_transaction
+
+import za.co.app.budgetbee.base.IBaseView
+import za.co.app.budgetbee.data.repository.IDatabaseRepository
+
+class EditTransactionPresenter(transactionsRepository: IDatabaseRepository) : IEditTransactionMvp.Presenter {
+    override fun attachView(view: IBaseView) {
+
+    }
+
+    override fun detachView() {
+
+    }
+
+}
