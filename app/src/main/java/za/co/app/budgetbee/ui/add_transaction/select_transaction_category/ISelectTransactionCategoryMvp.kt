ce7ec1a4352e3b5fe3601299d55fbb900ddaf2ed
joemalebe <jtekniq@gmail.com>
2020-10-15 21:26:15 +0200

    create select transaction category mvp interface

---
+++ app/src/main/java/za/co/app/budgetbee/ui/add_transaction/select_transaction_category/ISelectTransactionCategoryMvp.kt
@@ -0,0 +1,17 @@
+package za.co.app.budgetbee.ui.add_transaction.select_transaction_category
+
+import za.co.app.budgetbee.base.IBasePresenter
+import za.co.app.budgetbee.base.IServiceCallView
+import za.co.app.budgetbee.data.model.domain.TransactionCategory
+import za.co.app.budgetbee.data.model.domain.TransactionCategoryType
+import java.util.*
+
+interface ISelectTransactionCategoryMvp {
+    interface View : IServiceCallView{
+        fun displayCategories(transactionCategories: ArrayList<TransactionCategory>)
+    }
+
+    interface Presenter : IBasePresenter{
+        fun getAllTransactionCategoriesByType(transactionCategoryType: TransactionCategoryType)
+    }
+}
