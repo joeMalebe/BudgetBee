03bbde1bd9868e9825549164ff9572594ebc388b
joemalebe <jtekniq@gmail.com>
2021-09-15 13:05:07 +0200

    create balance report screen

---
+++ app/src/main/java/za/co/app/budgetbee/ui/report/BalanceReportActivity.kt
@@ -0,0 +1,12 @@
+package za.co.app.budgetbee.ui.report
+
+import android.os.Bundle
+import za.co.app.budgetbee.R
+import za.co.app.budgetbee.base.AppCompatBaseActivity
+
+class BalanceReportActivity : AppCompatBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_balance_report)
+    }
+}
