ec9ee6a40c4490e35a0b5a8757c76636ad2ea0bf
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-11-28 22:23:09 +0200

    TransactionsListAdapter replace synthetics with view binding

--- app/src/main/java/za/co/app/budgetbee/ui/landing/TransactionsListAdapter.kt
+++ app/src/main/java/za/co/app/budgetbee/ui/landing/TransactionsListAdapter.kt
@@ -3,32 +3,31 @@ package za.co.app.budgetbee.ui.landing
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.item_transaction.view.*
 import za.co.app.budgetbee.R
 import za.co.app.budgetbee.data.model.domain.Transaction
 import za.co.app.budgetbee.data.model.domain.TransactionCategoryType
+import za.co.app.budgetbee.databinding.ItemTransactionBinding
 import za.co.app.budgetbee.ui.landing.TransactionsListAdapter.TransactionsViewHolder
 import za.co.app.budgetbee.utils.displayLongDouble
 
 class TransactionsListAdapter(private val transactions: ArrayList<Transaction>) :
     RecyclerView.Adapter<TransactionsViewHolder>() {
     val publishTransaction = PublishSubject.create<Transaction>()
+    lateinit var binding: ItemTransactionBinding
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionsViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_transaction, parent, false)
-        return TransactionsViewHolder(view)
+        binding = ItemTransactionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TransactionsViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TransactionsViewHolder, position: Int) {
-        holder.display(transactions.get(position), isLastItem(position))
+        holder.display(transactions[position], isLastItem(position))
     }
 
     private fun isLastItem(position: Int): Boolean {
-        return position == getItemCount() - 1
+        return position == itemCount - 1
     }
 
     override fun getItemCount(): Int {
@@ -39,32 +38,25 @@ class TransactionsListAdapter(private val transactions: ArrayList<Transaction>) 
         return publishTransaction.hide()
     }
 
-    inner class TransactionsViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val transactionCategoryTextView: TextView =
-            view.findViewById(R.id.text_transaction_category)
-        val transactionDescriptionTextView: TextView =
-            view.findViewById(R.id.text_transaction_description)
-        val transactionAmountTextView: TextView =
-            view.findViewById(R.id.text_transaction_amount)
-        val divider: View = view.findViewById(R.id.transaction_divider)
+    inner class TransactionsViewHolder(val binding: ItemTransactionBinding) : RecyclerView.ViewHolder(binding.root) {
 
         val transactionDate = R.id.text_transaction_amount
         fun display(transaction: Transaction, islastItem: Boolean) {
-            view.apply {
+            binding.apply {
                 val isIncome = isIncomeTransaction(transaction)
-                transactionCategoryTextView.text = transaction.transactionCategoryName
-                transactionDescriptionTextView.text = transaction.transactionDescription
-                transactionAmountTextView.text = transaction.transactionAmount.displayLongDouble()
-                text_plus_minus_sign.setTextColor(itemView.resources.getColor(getTransactionColor(transaction)))
-                text_plus_minus_sign.text = if (isIncome) {
-                    image_arrow.setImageResource(R.drawable.ic_arrow_up)
-                    context.getString(R.string.plus)
+                textTransactionCategory.text = transaction.transactionCategoryName
+                textTransactionDescription.text = transaction.transactionDescription
+                textTransactionAmount.text = transaction.transactionAmount.displayLongDouble()
+                textPlusMinusSign.setTextColor(itemView.resources.getColor(getTransactionColor(transaction)))
+                textPlusMinusSign.text = if (isIncome) {
+                    imageArrow.setImageResource(R.drawable.ic_arrow_up)
+                   root.context.getString(R.string.plus)
                 } else {
-                    image_arrow.setImageResource(R.drawable.ic_arrow_down)
-                    context.getString(R.string.minus)
+                    imageArrow.setImageResource(R.drawable.ic_arrow_down)
+                    root.context.getString(R.string.minus)
                 }
 
-                divider.visibility = if (islastItem) View.GONE else View.VISIBLE
+                transactionDivider.visibility = if (islastItem) View.GONE else View.VISIBLE
                 itemView.setOnClickListener {
                     publishTransaction.onNext(transaction)
                 }
